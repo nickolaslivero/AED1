673c6de4ee794ce01e03c7fe9bb45c813483ba8e
nickolaslivero <njsl.snf21@uea.edu.br>
2023-06-20 19:08:09 -0400

    atividade05

---
+++ atividade05/recursion.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+long fatorial(long n)
+{
+    if ((n==1) || (n==0))
+        return 1;
+    else 
+        return (fatorial(n-1)*n);
+}
+
+long fatorial1(int n)
+{
+    long fat;
+    if ((n==1) || (n==0))
+        fat = 1;
+    else
+        for (fat = 1; n > 1; n--)
+            fat = fat * n;
+    return fat;
+}
+
+int main(int argc, char** argv){
+    int num;
+    printf("Digite um número: ");
+    scanf("%d", &num);
+    
+    long result1 = fatorial(num);
+    long result2 = fatorial1(num);
+    
+    printf("Fatorial recursivo': %ld\n", result1);
+    printf("Fatorial iterativo: %ld\n", result2);
+    
+    return 0;
+}
